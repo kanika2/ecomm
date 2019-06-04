2dee5e6d1b627a1ea00816d2be0486691df2a754
Kanika Thapliyal <kanikathapliyal17@gmail.com>
2019-06-04 13:15:14 +0530

    sldjkf

---
+++ precache-manifest.8b81ff528025a2d462325ac13524afd8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4190baad31030de396b50375d0501f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f9591e63812ccee8fbb",
+    "url": "/static/css/main.b31bad2e.chunk.css"
+  },
+  {
+    "revision": "8fe38e711acba3f433a5",
+    "url": "/static/js/2.901bec76.chunk.js"
+  },
+  {
+    "revision": "6f9591e63812ccee8fbb",
+    "url": "/static/js/main.d2217bee.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
